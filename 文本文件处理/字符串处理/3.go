c4fee2c121ab0be4a6f0879e0bd4d0cef4deff57
Geoffrey <1183681473@qq.com>
2020-12-22 10:18:38 +0800

    字符串处理

--- 文本文件处理/字符串处理/3.go
+++ 文本文件处理/字符串处理/3.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"strings"
 )
 
 func main() {
-	a := strconv.FormatBool(false)
-	b := strconv.FormatFloat(123.23, 'g', 12, 64)
-	c := strconv.FormatInt(1234, 10)
-	d := strconv.FormatUint(12345, 10)
-	e := strconv.Itoa(1023)
-	fmt.Println(a, b, c, d, e)
+	/*
+		fmt.Println(strings.Contains("seafood", "foo")) // true
+		s := []string{"foo", "bar", "baz"}
+		fmt.Println(strings.Join(s, ", ")) // foo, bar, baz
+
+		fmt.Println(strings.Index("lang go", "test")) // -1  如果找不到返回 -1
+
+		fmt.Println("ba" + strings.Repeat("na", 3))
+
+		%q      单引号围绕的字符字面值，由Go语法安全地转义 Printf("%q", 0x4E2D)        '中'
+		fmt.Printf("%q\n", strings.Split("a,b,c", ","))
+	*/
+	// Fields are: ["foo" "bar" "baz"]   Fields are: ["foobar" "baz"]
+	fmt.Printf("Fields are: %q", strings.Fields("  foobar  baz   ")) // 去除s字符串的空格符，并且按照空格分割返回slice
+
 }
